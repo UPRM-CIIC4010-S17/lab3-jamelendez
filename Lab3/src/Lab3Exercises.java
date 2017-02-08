21eb1faaf4d8eb58610c4619244ee5ad5b83cd32
3010a9 <3010a9@coco.ece.uprm.edu>
2017-02-08 16:21:48 -0400

    Changed the tilte of the frame to "Jose A. Melendez". Changed the size of the frame to 200 x 200. Changed the location of the frame to x = 850 and y = 450 (somewhat in the center of the monitor).

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Jose Melendez");
+		JFrame myFrame = new JFrame("Jose A. Melendez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(850, 450);
 		myFrame.setSize(200, 200);

25a5ff216a52894af538632de40ff4047e0ba084
Andrés <Andrs@10.31.38.121>
2017-02-09 15:36:15 -0400

    Added an arrow and removed lines and eclipse.

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Insets;
+import java.awt.Polygon;
+
 import javax.swing.JPanel;
  
 public class MyPanelClass extends JPanel {
@@ -33,13 +35,24 @@ public class MyPanelClass extends JPanel {
                         //g.setColor(Color.BLUE);
                         //g.drawRect(x1 + 4, y1 + 4, width - 8, height - 8);
                         
-                        g.setColor(Color.WHITE);
-                        g.drawLine(x1, y1, x2, y2);
+//                        g.setColor(Color.WHITE);
+//                        g.drawLine(x1, y1, x2, y2);
                         
-                        g.setColor(Color.RED);
-                        g.drawLine(x1, y2, x2, y1);
+//                        g.setColor(Color.RED);
+//                        g.drawLine(x1, y2, x2, y1);
                         
-                        g.setColor(Color.GRAY);
-                        g.fillOval((width/2)-(55/2), (height/2)- (55/2), 55, 55);
+                        //g.setColor(Color.GRAY);
+                        //g.fillOval((width/2)-(55/2), (height/2)- (55/2), 55, 55);
+                        
+                        Polygon p = new Polygon();
+                        p.addPoint(x1 + 5, y1 + 25);
+                        p.addPoint(x1 + 20, y1 + 10);
+                        p.addPoint(x1 + 35, y1 + 25);
+                        p.addPoint(x1 + 25, y1 + 25);
+                        p.addPoint(x1 + 25, y1 + 45);
+                        p.addPoint(x1 + 15, y1 + 45);
+                        p.addPoint(x1 + 15, y1 + 25);
+                        g.setColor(Color.BLACK);
+                        g.drawPolygon(p);
             }
 }
